8e7620ecbca00c780aabeebe8ef0c71a0144bb6c
WINDOWS-10 <WINDOWS-10@DESKTOP-VABLH7K>
2023-06-22 15:19:33 +0530

    PUSH

--- Demo/src/test/java/DemoPack/Demo.java
+++ Demo/src/test/java/DemoPack/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("I am working");
 		System.out.println("First pull");
-
+        System.out.println("First push");
 	}
 
 }
