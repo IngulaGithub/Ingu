7d960c48bd38dce24020c11e8ae8670019984a01
WINDOWS-10 <WINDOWS-10@DESKTOP-VABLH7K>
2023-06-22 15:16:37 +0530

    Committed

--- Demo/src/test/java/DemoPack/Demo.java
+++ Demo/src/test/java/DemoPack/Demo.java
@@ -1,9 +1,9 @@
-package cscscsc;
+package DemoPack;
 
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("cdaczdczc");
+		System.out.println("I am working");
 
 	}
 
